513e1f5771634a406bb58f9ec0ede0fd01d421db
kevinpossos117 <kevinpossos@gmail.com>
2025-04-28 01:28:11 -0500

    ejercicio 6

--- aqui.py
+++ aqui.py
@@ -76,6 +76,20 @@ B=float(input("ingrese el valor del billete(B)"))
 
 calcular_devueltas(P,M,H,B)
 
+def calcular_monto_final(P):
+    
+    tasa_interes = 0.03
+    meses = 2
+    M = P * (1 + tasa_interes) ** meses
+    return M
+
+P = float(input("Ingrese la cantidad prestada (P): "))
+
+monto_final = calcular_monto_final(P)
+print(f"El monto a pagar al final de dos meses es: {monto_final} pesos")
+
+
+
 
    
 
--- ejercicio4.py
+++ ejercicio4.py
@@ -1,49 +1,13 @@
 
-def calcular_devueltas(P, M, H, B):
- 
-    precio_de_pan = 300
-    precio_de_leche = 3300
-    precio_de_huevos = 350
+def calcular_monto_final(P):
+    
+    tasa_interes = 0.03
+    meses = 2
+    M = P * (1 + tasa_interes) ** meses
+    return M
 
+P = float(input("Ingrese la cantidad prestada (P): "))
 
-    costo_total = (P * precio_de_pan) + (M * precio_de_leche) + (H * precio_de_huevos)
-
-
-    diferencia = B - costo_total
-
-
-    if diferencia >= 0:
-        print(f"\nTe sobran {diferencia} pesos.")
-    else:
-        print(f"\nTe falta {-diferencia} pesos.")
-
-    print("\n¡Compra realizada exitosamente!")
-
-P = float(input("Ingrese la cantidad de panes (P): "))
-M = float(input("Ingrese la cantidad de bolsas de leche (M): "))
-H = float(input("Ingrese la cantidad de huevos (H): "))
-B = float(input("Ingrese el valor del billete (B): "))
-
-calcular_devueltas(P, M, H, B)
-
-def calcular_devueltas(P,M,H,B):
-   
-   precio_de_pan=300
-   precio_de_leche=3300
-   precio_de_huevos=350
-
-   costo_total=(P * precio_de_pan) + (M*precio_de_leche) + (H*precio_de_huevos) 
-   
-   diferecia= B -costo_total
-
-   if diferecia >= 0:
-      print(f"te sobran {diferecia}pesos.")
-   else:
-      print(f"te falta {-diferecia}pesos.") 
-
-P=float(input("ingrese la cantidad de p"))
-M=float(input("ingrese la cantidad de M"))
-H=float(input("ingrese la cantidad de H"))
-B=float(input("ingrese el valor del B"))
-
-calcular_devueltas(P,M,H,B)
+monto_final = calcular_monto_final(P)
+print(f"El monto a pagar al final de dos meses es: {monto_final} pesos")
+  
