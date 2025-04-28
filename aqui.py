da56309accb78e66705f6eb250cfba2a63c8c790
kevinpossos117 <kevinpossos@gmail.com>
2025-04-27 22:03:29 -0500

    ejercicio 2 terminado

--- aqui.py
+++ aqui.py
@@ -19,3 +19,17 @@ print(f"el volumen del solido es {volumen}")
 # tenemos que r1=3 y la formula de volumen de una esfera es 4/3*pi*r**3= entoces es 4/3*pi* 3**3=36pi
 #ahora el del cono tenemos que r2=4 y que h=9/2(4.5) y la formula es 1/3*pi*r**2*h=1/3*pi*4**2*(9/2)=24pi
 #entoces volumen total= volumen ezfera+ volumen cono=36pi+24pi=60pi=188.496
+
+import math
+
+def area_lateral_vagon(a, b, r):
+    area_rectangulo = a * b
+    area_rueda = math.pi * r ** 2
+    area_total = area_rectangulo + 2 * area_rueda
+    return area_total
+a=float(input("ingrese el valor de la a:"))
+b=float(input("ingrese el valor de la b:"))
+r=float(input("ingrese el valor de las r:"))
+
+print("el area lateral del vagon es",area_lateral_vagon(a,b,r))
+
